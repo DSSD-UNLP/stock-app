e86696ee5eb4c87da95ba28c93c7a0a2ff2caaae
Julian Pasquale <julian.pasquale@snappler.com>
2018-09-23 13:35:54 -0300

    changes in django_rest_framework use

--- stockProject/stockApp/serializer.py
+++ stockProject/stockApp/serializer.py
@@ -2,13 +2,13 @@ from stockProject.stockApp.models import Type, Product
 from rest_framework import serializers
 
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('name', 'cost_price', 'sale_price', 'product_type')
 
 
-class TypeSerializer(serializers.HyperlinkedModelSerializer):
+class TypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Type
         fields = ('name', 'description')
--- stockProject/stockApp/views.py
+++ stockProject/stockApp/views.py
@@ -1,18 +1,40 @@
 from stockProject.stockApp.models import Product, Type
-from rest_framework import viewsets
 from stockProject.stockApp.serializer import ProductSerializer, TypeSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductList(APIView):
     """
-    API endpoint that allows products to be viewed or edited.
+    API endpoint that allows products to be viewed or created.
+
+    /products/
+
     """
     queryset = Product.objects.all().order_by('-name')
     serializer_class = ProductSerializer
+    
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    @csrf_exempt
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class ProductDetail(APIView):
 
 
-class TypeViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows types to be viewed or edited.
-    """
-    queryset = Type.objects.all()
-    serializer_class = TypeSerializer
+# class TypeViewSet(APIView):
+#     """
+#     API endpoint that allows types to be viewed or edited.
+#     """
+#     queryset = Type.objects.all()
+#     serializer_class = TypeSerializer
