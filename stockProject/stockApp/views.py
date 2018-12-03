674e70e52d5d70c0199d36b42e3905d35cbe6422
Julian Pasquale <julian.pasquale@snappler.com>
2018-12-02 22:10:53 -0300

    last changes

--- stockProject/stockApp/views.py
+++ stockProject/stockApp/views.py
@@ -51,14 +51,15 @@ class ProductDetail(APIView):
         product.delete()
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
-    def put(self,request,pk):
+    def patch(self,request,pk):
         product = get_object_or_404(Product,pk=pk)
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-    def patch(self,request,pk):
+
+    def put(self,request,pk):
         product = get_object_or_404(Product,pk=pk)
         if product.stock > 0:
             product.stock = product.stock - 1
